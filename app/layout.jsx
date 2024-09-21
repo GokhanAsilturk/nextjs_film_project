da6060e1ae2c12d30c3d6fd7929dae7f89a26331
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2024-09-21 19:36:18 +0300

    started

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Inter} from '@next/font/google'
+import '../styles/reset.css'
+import '../styles/global.css';
+import Footer from '@/components/footer'
+import Header from '@/components/header';
+
+const interFontFamily = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr" className={interFontFamily.className}>
+      <body className="container">
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+      </body>
+      
+    </html>
+  );
+}
